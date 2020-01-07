bff60fca127351ba8070c1163831c8c6fc587c5a
Timothy <timaaaay@gmail.com>
2020-01-07 22:13:15 +1030

    Added SMS functionality
    Added SMS when domain available functionality
    SMS requires a free account from https://www.twilio.com/

--- dac.py
+++ dac.py
@@ -1,14 +1,30 @@
 import whois
 import traceback
+from twilio.rest import Client
+
+# Twilio Configuration
+# Enter your Twilio account details below. See https://www.twilio.com/console
+account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxx'
+auth_token = 'edxxxxxxxxxxxxxxxxxxxxxxx'
+client = Client(account_sid, auth_token)
+
+# Enter your domain below
+domain = 'example.com'
 
 def domain_lookup():
-    input = raw_input("Enter a domain: ")
-    w = whois.whois(input)
-    print("Checking domain: " + str(w.domain_name))
-    print("Expiry date: " + str(w.expiration_date)) # This prints Expiry date: [datetime.datetime(2021, 8, 16, 14, 15, 29)
-    print(w.expiration_date[0]) # This prints 2021-08-16 14:15:29
+    w = whois.whois(domain)
+    print("Checking domain: " + str(w.domain_name[0]))
+    print("Expiry date: " + str(w.expiration_date))
     
 try:
     domain_lookup()
 except:
-    print("Domain is not registered!")
+    print('Domain is unregistered. Sending SMS')
+    message = client.messages \
+        .create(
+        body= domain + ' is not registered. Click the following link to register the domain: https://www.crazydomains.com.au/ ',
+        # Enter your trial number below. See https://www.twilio.com/console for your trial number.
+        from_='+16xxxxxxxxx',
+        # Enter your Verified Caller ID below. See https://www.twilio.com/console/phone-numbers/verified
+        to='+61xxxxxxxxx'
+    )
