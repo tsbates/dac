27c369dff6ee1ed4db249cddc658cd42e6cf9759
Timothy <timaaaay@gmail.com>
2021-10-25 20:50:28 +1030

    Update dac.py

--- dac.py
+++ dac.py
@@ -1,30 +1,32 @@
 import whois
-import traceback
 from twilio.rest import Client
 
 # Twilio Configuration
 # Enter your Twilio account details below. See https://www.twilio.com/console
-account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxx'
-auth_token = 'edxxxxxxxxxxxxxxxxxxxxxxx'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 # Enter your domain below
-domain = 'example.com'
+domain = 'DOMAINHERE'
 
 def domain_lookup():
-    w = whois.whois(domain)
-    print("Checking domain: " + str(w.domain_name[0]))
-    print("Expiry date: " + str(w.expiration_date))
-    
-try:
-    domain_lookup()
-except:
-    print('Domain is unregistered. Sending SMS')
-    message = client.messages \
+    try:
+        w = whois.whois(domain)
+        print("Checking domain: " + str(w.domain_name))
+        print("Domain %s is registered!" % (domain))
+        print("Checking expiration date on " + str(w.domain_name) + "...")
+        print("Expiry date: " + str(w.expiration_date[0]))
+    except:
+        print("Domain %s is unregistered! Sending SMS" % (domain))
+        message = client.messages \
         .create(
         body= domain + ' is not registered. Click the following link to register the domain: https://www.crazydomains.com.au/ ',
         # Enter your trial number below. See https://www.twilio.com/console for your trial number.
-        from_='+16xxxxxxxxx',
+        from_='+XXXXXXXXXXX',
         # Enter your Verified Caller ID below. See https://www.twilio.com/console/phone-numbers/verified
-        to='+61xxxxxxxxx'
+        to='+XXXXXXXXXXX'
     )
+ 
+if __name__ == '__main__':
+    domain_lookup()
